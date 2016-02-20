b0f6b0d8603ab943f1feb66626febbdfa5645d6c
github-chenzhou <964412049@qq.com>
2016-02-20 18:21:31 +0800

    add

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var Route = ReactRouter.Route;
 var browserHistory = ReactRouter.browserHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 
-
 var Nav = require('./components/canvasside.jsx');
 var Movies = require('./page/movies.jsx');
 var Movieinfo = require('./page/movieinfo.jsx');
@@ -58,7 +57,14 @@ ReactDOM.render((
       <Route path="books" component={{list: Books}} />
       <Route path="book/:id" component={{list: Books, info: Book}}/>
     </Route>
-    <Route path="/react-doushen" component={App}>
+    <Route path="/index.html" component={App}>
+      <IndexRoute component={{list: Movies}} />
+      <Route path="movies" component={{list: Movies}} />
+      <Route path="movie/:id" component={{list: Movies, info: Movieinfo}}/>
+      <Route path="books" component={{list: Books}} />
+      <Route path="book/:id" component={{list: Books, info: Book}}/>
+    </Route>
+    <Route path="/react-doushen/" component={App}>
       <IndexRoute component={{list: Movies}} />
       <Route path="movies" component={{list: Movies}} />
       <Route path="movie/:id" component={{list: Movies, info: Movieinfo}}/>
